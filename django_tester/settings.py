16da3855ca70d900c52befc87357bdc8d6dd1718
Vadim-2002 <vadim.winter2@mail.ru>
2024-07-06 14:29:25 +0500

    Перешли на удаленную базу данны postgresql

--- django_tester/settings.py
+++ django_tester/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_tester.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'WVuokfBPGUsneeCxOEUxCrpxwFJLOiZU',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '56908',
     }
 }
 
